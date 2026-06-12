f98648617d849afa312340b2675777e1ef57f0b6
tamaina <tamaina@hotmail.co.jp>
2026-06-12 13:34:43 +0900

    絶対URLにする

--- packages/app/src/worker/routes/embed.ts
+++ packages/app/src/worker/routes/embed.ts
@@ -2,6 +2,6 @@ import { Hono } from 'hono';
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/e/*', (c) => c.env.ASSETS.fetch('/embed'));
+app.get('/e/*', (c) => c.env.ASSETS.fetch(new Request(new URL('/embed', c.req.url))));
 
 export const embedRoutes = app;
